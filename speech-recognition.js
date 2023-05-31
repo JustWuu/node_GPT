626a8950e825d199bcce24936406354220616762
ZongWen Cai <wu@acube.com.tw>
2023-05-31 10:21:21 +0800

    feat(init): 專案上傳

---
+++ speech-recognition.js
@@ -0,0 +1,41 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const { joinVoiceChannel } = require("@discordjs/voice");
+const { addSpeechEvent } = require("discord-speech-recognition");
+
+
+// MTExMjk4NTQ1NTMzNjYyMDA5Mw.GUw6gP.PHzerOAAtczbg1K3Gfw_WFoaLZfrSmvvSaNMQQ
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+addSpeechEvent(client);
+
+client.on("messageCreate", (msg) => {
+  const voiceChannel = msg.member?.voice.channel;
+  if (voiceChannel) {
+    joinVoiceChannel({
+      channelId: voiceChannel.id,
+      guildId: voiceChannel.guild.id,
+      adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+      selfDeaf: false,
+    });
+  }
+});
+
+client.on("speech", (msg) => {
+  // If bot didn't recognize speech, content will be empty
+  if (!msg.content) return;
+
+  msg.author.send(msg.content);
+});
+
+client.on("ready", () => {
+  console.log("Ready!");
+});
+
+client.login("token");
