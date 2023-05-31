7be136b37f05ebf714a8e72b80aa7ef968b4581b
ZongWen Cai <wu@acube.com.tw>
2023-05-31 12:17:18 +0800

    feat(GPT): 調整

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
-const line = require('@line/bot-sdk');
 const express = require('express');
+const line = require('@line/bot-sdk');
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
@@ -9,11 +9,10 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-
 // create LINE SDK config from env variables
 const config = {
-  channelAccessToken: process.env.LIN_BOT_TOKEN,
-  channelSecret: process.env.LIN_BOT_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
 };
 
 // create LINE SDK client
@@ -37,32 +36,24 @@ app.post('/callback', line.middleware(config), (req, res) => {
 
 // event handler
 async function handleEvent(event) {
-
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
 
-  // if(event.message.text.indexOf('豆花') < 0){
-  //   return Promise.resolve(null);
-  // }
-
-  const completion = await openai.createChatCompletion({
+  const { data } = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: [
       {
-      role: 'user',
-      content: event.message.text,
-      },
-      // {
-      //   role: 'system',
-      //   content: '你好，我是機器人',
-      // },
+        role: 'user',
+        content: event.message.text,
+      }
     ],
     max_tokens: 200,
   });
 
   // create a echoing text message
+  const [choices] = data.choices;
   const echo = { type: 'text', text: choices.message.content.trim() || '抱歉，我沒有話可說了。' };
 
   // use reply API
