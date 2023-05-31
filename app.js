23614536a28f95f233c59c18241d9e6a8aaafa79
ZongWen Cai <wu@acube.com.tw>
2023-05-31 11:50:07 +0800

    feat(GPT): 串OpenAI的API

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ require('dotenv').config();
 
 const line = require('@line/bot-sdk');
 const express = require('express');
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 
 // create LINE SDK config from env variables
 const config = {
@@ -29,14 +36,34 @@ app.post('/callback', line.middleware(config), (req, res) => {
 });
 
 // event handler
-function handleEvent(event) {
+async function handleEvent(event) {
+
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
 
+  if(event.message.text.indexOf('豆花') < 0){
+    return Promise.resolve(null);
+  }
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+      role: 'user',
+      content: event.message.text,
+      },
+      {
+        role: 'system',
+        content: '你好，我是機器人',
+      },
+    ],
+    max_tokens: 200,
+  });
+
   // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
+  const echo = { type: 'text', text: choices.message.content.trim() || '抱歉，我沒有話可說了。' };
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
