a52a0fac8313a94308465d9ce156092392e65a71
ZongWen Cai <wu@acube.com.tw>
2023-06-01 11:25:21 +0800

    fix(firebase): 資料庫js改內容

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-// let firebase = require('firebase'); //引入firebase
 const firebase = require("firebase/app");
 const databse = require('firebase/database')
 
@@ -13,29 +12,17 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
-
-
 const data = databse.getDatabase()
 
 function pushMessage(name, message, echo){
   const key = databse.push(databse.ref(data, 'GPT')).key;
   databse.update(databse.ref(data, `GPT/${key}`),{
-    text:'asd'
+    lineName:name,
+    message:message,
+    echo:echo,
   })
   
 }
 
 module.exports = pushMessage
-
-// const firebaseConfig = {
-//     apiKey:process.env.FB_API_KEY,
-//     authDomain:process.env.FB_AUTH_DOMAIN,
-//     projectId:process.env.FB_PROJECT_ID,
-//     storageBucket:process.env.FB_STORAGE_BUCKET,
-//     messagingSenderId:process.env.FB_MESSAGING_SENDER_ID,
-//     appId:process.env.FB_APP_ID,
-// };
-
-// firebase.initializeApp(firebaseConfig); //Initialize Firebase
-
 
